a841f894c690a1c5ca761602636e305821e4d208
Shudong Zhang <zhangshudong@gmail.com>
2019-08-27 20:10:57 +0800

    Initial commit

---
+++ src/main/kotlin/com/shudong/kotlin/demo/DemoApplication.kt
@@ -0,0 +1,14 @@
+package com.shudong.kotlin.demo
+
+import org.springframework.boot.Banner.Mode.OFF
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args) {
+        setBannerMode(OFF)
+    }
+}
