c9014b686fc5d394a9b6695c4b948c25637a2284
Shudong Zhang <zhangshudong@gmail.com>
2019-08-29 00:19:04 +0800

    Add PersonRepository

--- src/main/kotlin/com/shudong/kotlin/demo/repository/PersonRepository.kt
+++ src/main/kotlin/com/shudong/kotlin/demo/repository/PersonRepository.kt
@@ -1,9 +1,24 @@
 package com.shudong.kotlin.demo.repository
 
+import org.hibernate.annotations.GenericGenerator
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.Version
 
-data class Person(var id: Int?, val name: String, val age: Int)
+@Entity
+data class Person(
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name="uuid", strategy = "uuid2")
+    val id: String? = null,
+    val name: String,
+    val age: Int,
+    @Version
+    val version: Long = 0
+)
 
 @Repository
-class PersonRepository {
-}
+interface PersonRepository: CrudRepository<Person, String>
--- src/main/kotlin/com/shudong/kotlin/demo/service/PersonService.kt
+++ src/main/kotlin/com/shudong/kotlin/demo/service/PersonService.kt
@@ -2,17 +2,20 @@ package com.shudong.kotlin.demo.service
 
 import com.shudong.kotlin.demo.logger
 import com.shudong.kotlin.demo.repository.Person
+import com.shudong.kotlin.demo.repository.PersonRepository
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
-class PersonService {
+class PersonService(private val personRepository: PersonRepository) {
 
     companion object {
         private val logger = LoggerFactory.getLogger(PersonService::class.java)
     }
 
     fun findAll(): List<Person> {
+        var person = personRepository.save(Person(name = "shudong", age = 35))
+        println(person)
         logger.info("findAll");
         logger().info("findAll2")
         println(logger === logger())
